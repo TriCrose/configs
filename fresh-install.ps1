d9611be1cde8269f44edc9c47764ddfd4aba3ad9
Tristram Newman <tristram.newman@gmail.com>
2020-12-14 21:40:15 +0000

    Add fresh-install.ps1

---
+++ fresh-install.ps1
@@ -0,0 +1,7 @@
+# Install chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install software
+cinst bat chromium everything firefox fzf git neovim neovide powershell-core ripgrep microsoft-windows-terminal expressvpn discord deluge eraser steam spotify vlc
