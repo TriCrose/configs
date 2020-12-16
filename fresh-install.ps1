7d5de6a0451d01df4f307355cdfc072e21ecae2c
Tristram Newman <tristram.newman@gmail.com>
2020-12-16 00:05:34 +0000

    Add poshgit

--- fresh-install.ps1
+++ fresh-install.ps1
@@ -4,4 +4,4 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Install software
-cinst bat chromium everything firefox fzf 7zip git neovim neovide powershell-core ripgrep microsoft-windows-terminal expressvpn discord deluge eraser steam spotify vlc logitech-options firacode nodejs
+cinst bat chromium everything firefox fzf 7zip git poshgit neovim neovide powershell-core ripgrep microsoft-windows-terminal expressvpn discord deluge eraser steam spotify vlc logitech-options firacode nodejs
